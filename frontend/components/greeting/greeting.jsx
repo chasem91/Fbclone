4ede6921dc8699549060459d3e8ff404f2ee09a9
Chase Martin <chasemartin91@gmail.com>
2016-10-11 22:19:18 -0400

    Set small logo background image

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -21,13 +21,13 @@ export default class Greeting extends React.Component {
     return (
       <div className="personal-greeting group">
         <div className="personal-greeting-elements">
-          <NotificationsContainer />
-          <FriendRequestsContainer />
-          <span className="small-logo">F</span>
+          <div className="small-logo"></div>
           <SearchBarContainer />
-          <Link to={`/`} onClick={this.props.logout} className="header-button">Log Out</Link>
           <Link to={`/users/${this.props.currentUser.id}`} activeClassName="current">Profile</Link>
           <Link to={`/newsfeed`} activeClassName="current">Newsfeed</Link>
+          <FriendRequestsContainer />
+          <NotificationsContainer />
+          <Link to={`/`} onClick={this.props.logout} className="header-button">Log Out</Link>
         </div>
       </div>
     );
