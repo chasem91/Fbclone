4ee44da82947ec9239540fa6d4593d463a7dea2e
Chase Martin <chasemartin91@gmail.com>
2016-12-10 13:11:15 -0500

    Optimize for production

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/fb_clone.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
